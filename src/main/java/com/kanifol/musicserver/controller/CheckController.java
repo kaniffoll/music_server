6fe5c76f3b7212151ef8b05f2cf0c51caf0c3ce2
kaniffoll <stakot2902@gmail.com>
2026-04-21 17:27:18 +0300

    CheckController added for check auth

---
+++ src/main/java/com/kanifol/musicserver/controller/CheckController.java
@@ -0,0 +1,16 @@
+package com.kanifol.musicserver.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "check")
+public class CheckController {
+
+   @GetMapping
+   public ResponseEntity<String> check() {
+       return ResponseEntity.ok("OK");
+   }
+}
